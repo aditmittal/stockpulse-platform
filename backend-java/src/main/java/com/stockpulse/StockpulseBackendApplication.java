e886b4fe52d8e6017a1656ce744c756d2016ae14
adit mittal <adit11041998@gmail.com>
2025-07-02 18:38:05 +0530

    backend skeleton

---
+++ backend-java/src/main/java/com/stockpulse/StockpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stockpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpulseBackendApplication.class, args);
+	}
+
+}
